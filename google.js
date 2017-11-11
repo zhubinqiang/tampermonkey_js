aa680f31189a113634ab39c3b3b9c7516df333a9
zhubinqiang <zhubinqiang@gmail.com>
2017-11-11 22:34:34 +0800

    add google

---
+++ google.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         google
+// @namespace    https://github.com/zhubinqiang/tampermonkey_js
+// @version      0.1
+// @description  open with a new tab in google search
+// @author       Zhu,binqiang
+// @match        https://www.google.com.hk/*
+// @grant        none
+// @require      https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    $().ready(function(){
+        console.log("aaaaa");
+        $("#ires").on("click","a", function() {
+            $(this).attr("target", "_blank");
+        });
+
+        $("#rhs_block").on("click","a", function() {
+            $(this).attr("target", "_blank");
+        });
+
+    });
+
+})();
