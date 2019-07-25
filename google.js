e2d9060f1707b0b375b2e5e61d9ce05ff0c5f484
zhubinqiang <zhubinqiang@gmail.com>
2019-07-25 09:16:07 +0800

    update google

--- google.js
+++ google.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         google
 // @namespace    https://github.com/zhubinqiang/tampermonkey_js
-// @version      0.1
+// @version      0.2
 // @description  open with a new tab in google search
 // @author       Zhu,binqiang
-// @match        https://www.google.com/*
 // @match        https://www.google.com.hk/*
+// @match        https://www.google.com/*
 // @grant        none
 // @require      https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js
 // ==/UserScript==
@@ -14,7 +14,9 @@
     'use strict';
 
     // Your code here...
-    $().ready(function() {
+    $().ready(function(){
+        /*
+        console.log("aaaaa");
         $("#ires").on("click","a", function() {
             $(this).attr("target", "_blank");
         });
@@ -22,7 +24,11 @@
         $("#rhs_block").on("click","a", function() {
             $(this).attr("target", "_blank");
         });
+        */
 
+        $(".g").on("click","a", function() {
+            $(this).attr("target", "_blank");
+        });
     });
 
 })();
